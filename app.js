d40b10d0428facae605dd8825ea0fbbf2cd8c4cb
Danith Dasun Hirunaka <d.dasunsera@gmail.com>
2025-06-21 16:52:18 +0530

    add search function

---
+++ app.js
@@ -0,0 +1,8 @@
+function searchCountry() {
+  console.log("searching...");
+  const countryValue = document.getElementById("searchValue").value;
+  if (!searchValue.value) {
+    alert("Please enter country name");
+    return;
+  }
+}
